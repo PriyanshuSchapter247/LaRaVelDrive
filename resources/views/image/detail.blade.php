34b810b6eefc82bd666dafdf27d6df9450ac89d2
PriyanshuSchapter247 <101408868+PriyanshuSchapter247@users.noreply.github.com>
2022-04-28 18:31:21 +0530

    Update detail.blade.php

--- resources/views/image/detail.blade.php
+++ resources/views/image/detail.blade.php
@@ -13,54 +13,6 @@
                                     <h2 class="featurette-heading">Image View<span class="text-muted"></span></h2>
                                 </center>
                             </div>
-{{--                            @if($image->created_by == auth()->user()->id)--}}
-
-
-{{--                                <div class="col-md-12" >--}}
-{{--                                    <center> <img style="height:10%; width:10%" src={{ asset('images/images/' . $image->image) }} alt=""></center>--}}
-{{--                                </div>--}}
-
-
-{{--                                <div class="col-md-6 mt-6" style="margin-left:25%; background-color:darkolivegreen">--}}
-{{--                                    <td>--}}
-{{--                                        <input type="text"  class="form-control" onclick="this.select()" value="{{asset('images/images/'.$image->image)}}" />--}}
-{{--                                    </td>--}}
-{{--                                </div>--}}
-
-{{--                            @if($share->send_to == auth()->user()->email || $share->send_from == auth()->user()->email  )--}}
-
-{{--                                <div class="col-md-12" >--}}
-{{--                                    <center> <img style="height:10%; width:10%" src={{ asset('images/images/' . $image->image) }} alt=""></center>--}}
-{{--                                </div>--}}
-
-
-{{--                                <div class="col-md-6 mt-6" style="margin-left:25%; background-color:darkolivegreen">--}}
-{{--                                    <td>--}}
-{{--                                        <input type="text"  class="form-control" onclick="this.select()" value="{{asset('images/images/'.$image->image)}}" />--}}
-{{--                                    </td>--}}
-{{--                                </div>--}}
-
-{{--                                @if (Auth::user()->id == $image->created_by)--}}
-{{--                                    <div class="col-md-12 mt-2">--}}
-{{--                                        <a href="/shareview/{{ $image->id }}"><i class="btn btn-block btn-outline-info"--}}
-{{--                                                                                 data-feather="edit">Share</i></a>--}}
-{{--                                    </div>--}}
-{{--                                @endif--}}
-
-
-{{--                            @else--}}
-{{--                                <div class="col-md-12 mt-3">--}}
-{{--                                    <center>--}}
-{{--                                        <h4><b> Not Allow by Owner.Plese send Request</b></h4>--}}
-{{--                                    </center>--}}
-{{--                                    <a href="/request/{{ $image->id }}"><i class="btn btn-block btn-outline-info"--}}
-{{--                                            data-feather="edit">Request</i></a>--}}
-{{--                                </div>--}}
-
-{{--                            @endif--}}
-{{--                                @endif--}}
-
-
 
                             @if (Auth::user()->id == $image->created_by)
 {{--                                 Statements inside body of if --}}
